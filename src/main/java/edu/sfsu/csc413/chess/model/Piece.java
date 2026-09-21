a3d5e177a6e2da3e71bca2f19d29cdde9e985323
johnny603 <johnnysantamaria603@gmail.com>
2026-09-21 11:25:28 -0700

    m2: refactor(model): refactored piece class using notes from session 7 and testing, preparing for m2

--- src/main/java/edu/sfsu/csc413/chess/model/Piece.java
+++ src/main/java/edu/sfsu/csc413/chess/model/Piece.java
@@ -1,14 +1,37 @@
 package edu.sfsu.csc413.chess.model;
+import java.util.List;
 
-public class Piece {
+
+public abstract class Piece {
     // remember color first, subclasses depend on order
     private final Color color;
     private final PieceType type;
 
     // constructor
-    public Piece(Color color, PieceType type) {
+    protected Piece(Color color, PieceType type) {
         this.color = color;
         this.type = type;
+    }
+
+    public abstract List<Move> pseudoLegalMoves(Board board, Position from);
+
+    // Pawn would Override this as it attacks differently
+    /** True if this piece could capture an enemy standing on {@code target}. */
+    public boolean attacks(Board board, Position from, Position target) {
+        for (Move move : pseudoLegalMoves(board, from)) {
+            if (move.to().equals(target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected List<Move> slidingMoves(Board board, Position from, int[][] directions) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    protected List<Move> steppingMoves(Board board, Position from, int[][] offsets) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     // accessors
@@ -24,3 +47,4 @@ public class Piece {
     @Override
     public String toString() { return String.valueOf(symbol()); }
 }
+
